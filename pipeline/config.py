0d06764b789bd2e6bee6b650cdcb0e4fea0e5b2b
berleon <github@leon-sixt.de>
2016-06-04 18:16:12 +0200

    Remove old config module

--- pipeline/config.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-# TODO: use some kind of config file / module
-
-saliency_threshold = 0.5
-saliency_model_path = '/home/ben/saliency-localizer-models/season_2015/'
-decoder_model_path = '/home/ben/decoder_architecture.json'
-decoder_weigths_path = '/home/ben/decoder_weights.h5'
