1a447949ea30a2358f1bc3ab0e3d4d65aa82cb30
lkairies <lkairies90@gmail.com>
2016-07-21 12:01:48 +0000

    add tagSimilarityEncoder model and weights

--- get_test_models.sh
+++ get_test_models.sh
@@ -4,17 +4,22 @@ set -e
 decoder_weights='https://github.com/BioroboticsLab/bb_decoder_models/raw/master/decoder_weights.h5'
 decoder_model='https://raw.githubusercontent.com/BioroboticsLab/bb_decoder_models/master/decoder_architecture.json'
 localizer_weights='https://github.com/nebw/saliency-localizer-models/raw/master/season_2015/saliency_weights-wobn'
+tagSimilarityEncoder_model='https://raw.githubusercontent.com/lkairies/bb_tagSimilarityEncoder_model/master/tagSimilarityEncoder_architecture.json'
+tagSimilarityEncoder_weights='https://github.com/lkairies/bb_tagSimilarity_Encoder_model/raw/master/tagSimilarityEncoder_weights.h5'
 
 decoder_dir="test/models/decoder"
 localizer_dir="test/models/localizer"
-
+tagSimilarityEncoder_dir="test/models/tagSimilarityEncoder"
 
 mkdir -p  $localizer_dir
 mkdir -p  $decoder_dir
+mkdir -p  $tagSimilarityEncoder_dir
 
 decoder_weights_fname="$decoder_dir/decoder_weights.hdf5"
 decoder_model_fname="$decoder_dir/decoder_architecture.json"
 localizer_weights_fname="$localizer_dir/saliency-weights.hdf5"
+tagSimilarityEncoder_weights_fname="$tagSimilarityEncoder_dir/tagSimilarityEncoder_weights.h5"
+tagSimilarityEncoder_model_fname="$tagSimilarityEncoder_dir/tagSimilarityEncoder_architecture.json"
 
 if [ ! -e "$decoder_weights_fname" ]; then
     echo "Downloading decoder weights"
@@ -29,4 +34,9 @@ fi
 if [ ! -e "$localizer_weights_fname" ]; then
     echo "Downloading localizer weights"
     curl -L $localizer_weights > $localizer_weights_fname
+fi
+
+if [ ! -e "$tagSimilarityEncoder_weights_fname" ]; then
+    echo "Downloading tagSimilarityEncoder weights"
+    curl -L $tagSimilarityEncoder_weights > $tagSimilarityEncoder_weights_fname
 fi
