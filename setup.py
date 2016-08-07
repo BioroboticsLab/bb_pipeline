aadf1dae78462db1dd66a10d35884ddec0cc4f5c
berleon <github@leon-sixt.de>
2016-08-07 15:40:52 +0200

    Use entry_points for scripts

--- setup.py
+++ setup.py
@@ -10,8 +10,12 @@ setup(
     name='bb_pipeline',
     version='0.0.1',
     description='',
-    scripts=[pjoin('bin', 'bb_pipeline'),
-             pjoin('bin', 'bb_pipeline_batch')],
+    entry_points={
+        'console_scripts': [
+            'bb_pipeline = pipeline.scripts.bb_pipeline:main',
+            'bb_pipeline_batch = pipeline.scripts.bb_pipeline_batch:main',
+        ]
+    },
     install_requires=reqs,
     dependency_links=dep_links,
     packages=['pipeline'],
