ba64b29631b7d4c38f7fe3a97743526c67f8cff3
Benjamin Wild <b.w@fu-berlin.de>
2017-09-07 15:35:02 +0200

    PEP8

--- pipeline/scripts/bb_pipeline_api.py
+++ pipeline/scripts/bb_pipeline_api.py
@@ -76,7 +76,7 @@ def png_encode(instance):
     Args:
         instance (ndarray): numpy array containing
                             the image to be converted
-    
+
     Returns:
         bytes: bytes containing the PNG-encoded image.
     """
@@ -85,7 +85,7 @@ def png_encode(instance):
         imsave(b, instance, 'png')
         return b.getvalue()
     return instance
-    
+
 
 def process_image(pipeline, image, png):
     """Helper function to execute a pipeline and get the results.
@@ -102,7 +102,8 @@ def process_image(pipeline, image, png):
     pipeline_results = pipeline([image])
     results_dict = {}
     for (k, v) in pipeline_results.items():
-        results_dict[k.__name__] = (png_encode(v) if (k.__name__ in png) else v.tolist())
+        results_dict[k.__name__] = (png_encode(
+            v) if (k.__name__ in png) else v.tolist())
     return msgpack.packb(results_dict)
 
 
@@ -110,7 +111,7 @@ def process_image(pipeline, image, png):
 def api_message():
     """This function handles the `/process` URL call.
     An image is appended as data to the request and the result is returned.
-    
+
     The desired output objects can be specified by assigning a JSON list
     to the optional `output` URL parameter (remember to percent-encode
     the string first). If omitted, the default output will be returned
@@ -120,11 +121,11 @@ def api_message():
     e.g. 'CrownOverlay') can be assigned to the optional `png` URL parameter,
     also as a JSON list.
 
-    
+
     Example:
-    
+
     .. code::
-    
+
         import requests
         import json
         from urllib import parse
@@ -150,12 +151,12 @@ def api_message():
     The results can be loaded like this:
 
     .. code::
-        
+
         # Deserialize results
         result_unpacked = msgpack.unpackb(result.content)
         ids_list = result_unpacked[b'IDs']
         crownoverlay_image = imread(io.BytesIO(result_unpacked[b'CrownOverlay']))
-    
+
     Note:
         The keys in the result dict are binary, since msgpack does not
         support string keys.
@@ -184,7 +185,8 @@ def api_message():
                         return 'Invalid pipeline output specified: {}'.format(o)
                     else:
                         output_objects.append(c)
-            print('Specified output: {}'.format([o.__name__ for o in output_objects]))
+            print('Specified output: {}'.format(
+                [o.__name__ for o in output_objects]))
             output = frozenset(output_objects)
             pipeline = get_cached_pipeline(output)
     png_please = request.args.get('png')
@@ -192,7 +194,8 @@ def api_message():
         png = []
     else:
         png = json.loads(parse.unquote(png_please))
-        if png: print('Specified png-encoded output: {}'.format([p for p in png]))
+        if png:
+            print('Specified png-encoded output: {}'.format([p for p in png]))
     if request.headers['Content-Type'] == 'application/octet-stream':
         try:
             with NamedTemporaryFile(delete=True) as f:
