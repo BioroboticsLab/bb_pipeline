029461fa60561937b267185b9a77cf6d629a9fe5
Leon Sixt <github@leon-sixt.de>
2018-08-07 10:56:07 +0200

    Fix pep8

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
-try: # for pip >= 10
+try:  # for pip >= 10
     from pip._internal.req import parse_requirements
-except ImportError: # for pip <= 9.0.3
+except ImportError:  # for pip <= 9.0.3
     from pip.req import parse_requirements
 
 install_reqs = parse_requirements('requirements.txt', session=False)
