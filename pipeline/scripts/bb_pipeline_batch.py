defa6773a9ca60be907922d2df3782040d8597fb
David Dormagen <czapper@gmx.de>
2019-05-22 10:09:38 +0200

    batch processing helper: add glob-matching argument and call the downstream function directly instead of over system call

--- pipeline/scripts/bb_pipeline_batch.py
+++ pipeline/scripts/bb_pipeline_batch.py
@@ -2,18 +2,20 @@
 
 import argparse
 import os
+import pathlib
 import shutil
 from itertools import chain
 from subprocess import call
 
-from pipeline.cmdline import logger, get_shared_positional_arguments, get_shared_optional_arguments
-
+from ..cmdline import logger, get_shared_positional_arguments, get_shared_optional_arguments
+from .bb_pipeline import process_video
 
 def main():  # pragma: no cover
     parser = argparse.ArgumentParser(
         prog='BeesBook pipeline batch processor',
         description='Batch process video using the beesbook pipeline')
 
+    parser.add_argument('--video_glob_pattern', help='glob pattern for files to process', type=str, default=None)
     parser.add_argument('video_root_path', help='root path of input videos', type=str)
     for arg, kwargs in chain(get_shared_positional_arguments(), get_shared_optional_arguments()):
         parser.add_argument(arg, **kwargs)
@@ -30,18 +32,20 @@ def main():  # pragma: no cover
     video_files = []
     for root, dirs, files in os.walk(args.video_root_path):
         for file in files:
-            video_files.append(os.path.join(root, file))
+            # Ignore timestamp files.
+            if not file.endswith("txt"):
+                full_path = os.path.join(root, file)
+                # Ignore files that do not match the given pattern.
+                if args.video_glob_pattern and args.video_glob_pattern != "None":
+                    if not pathlib.PurePath(full_path).match(args.video_glob_pattern):
+                        continue
+                video_files.append(full_path)
 
-    logger.info('Processing files: \n\t{}'.format('\n\t'.join(video_files)))
+    logger.info('Processing {} files: \n\t{}'.format(len(video_files), '\n\t'.join(video_files)))
 
     for fname in video_files:
-        cmd_args = [fname] + \
-            list([args.__dict__[param] for param, _ in get_shared_positional_arguments()])
-        for param, _ in get_shared_optional_arguments():
-            if param[2:] in args.__dict__:
-                cmd_args.append(param)
-                cmd_args.append(str(args.__dict__[param[2:]]))
-        call([pipeline_cmd] + cmd_args)
+        args.video_path = fname
+        process_video(args)
 
 
 if __name__ == '__main__':
